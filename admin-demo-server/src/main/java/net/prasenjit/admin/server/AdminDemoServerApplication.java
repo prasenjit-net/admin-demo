9222824be86c69313ff1942ea93d790f82620ea1
Prasenjit Purohit <github@prasenjit.net>
2015-12-02 02:59:33 +0530

    eureka added also eureka related changes done

--- admin-demo-server/src/main/java/net/prasenjit/admin/server/AdminDemoServerApplication.java
+++ admin-demo-server/src/main/java/net/prasenjit/admin/server/AdminDemoServerApplication.java
@@ -3,9 +3,11 @@ package net.prasenjit.admin.server;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
 public class AdminDemoServerApplication {
 
     public static void main(String[] args) {
