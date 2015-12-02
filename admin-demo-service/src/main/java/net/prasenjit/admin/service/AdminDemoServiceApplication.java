fbc5aae7572fc96b14f3d16cf0b72f3a702e4213
prasenjit-net <github@prasenjit.net>
2015-12-02 16:38:02 +0530

    5 log statement in different level added

--- admin-demo-service/src/main/java/net/prasenjit/admin/service/AdminDemoServiceApplication.java
+++ admin-demo-service/src/main/java/net/prasenjit/admin/service/AdminDemoServiceApplication.java
@@ -3,12 +3,28 @@ package net.prasenjit.admin.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AdminDemoServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(AdminDemoServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDemoServiceApplication.class, args);
+	}
+
+	@RequestMapping
+	public String home() {
+		log.trace("a trace message");
+		log.debug("a debug message");
+		log.info("a info message");
+		log.warn("a warn message");
+		log.error("a error message");
+		return "Hello Service";
+	}
 }
